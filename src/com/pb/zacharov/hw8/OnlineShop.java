0bf536cacb672942b992a15751872d711817f05d
danisz118 <daniz118@gmail.com>
2021-11-22 17:40:35 +0200

    add try catch

--- src/com/pb/zacharov/hw8/OnlineShop.java
+++ src/com/pb/zacharov/hw8/OnlineShop.java
@@ -3,7 +3,7 @@ package com.pb.zacharov.hw8;
 import java.util.Scanner;
 
 public class OnlineShop {
-    public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
+    public static void main(String[] args){
         Auth auth = new Auth();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введи логин: ");
@@ -12,13 +12,23 @@ public class OnlineShop {
         String password = scanner.nextLine();
         System.out.print("Повтори пароль: ");
         String confirmPassword = scanner.nextLine();
-        auth.signUp(login, password, confirmPassword);
+        try {
+            auth.signUp(login, password, confirmPassword);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            e.printStackTrace();
+            System.out.println("Пробуй еще раз ");
+        }
         System.out.println("Нового пользователя создано успешно \nАвторизация");
         System.out.print("Введите логин: ");
         login = scanner.nextLine();
         System.out.print("Введите пароль: ");
         password = scanner.nextLine();
-        auth.signIn(login, password);
+        try {
+            auth.signIn(login, password);
+        } catch (WrongLoginException | WrongPasswordException e) {
+            e.printStackTrace();
+            System.out.println("Пробуй еще раз ");
+        }
         System.out.println("Поздравляю  " + login + " в системе!");
         scanner.close();
     }
